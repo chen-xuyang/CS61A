816ce6d9b47ddb12501aec3472e03c42023fc1d4
ほむら <chenxuyang@mail.ustc.edu.cn>
2022-08-17 11:31:26 +0800

    🎈

---
+++ discs/disc07.py
@@ -0,0 +1,99 @@
+class MinList:
+    """A list that can only pop the smallest element """
+
+    def __init__(self):
+        self.items = []
+        self.size = 0
+
+    def append(self, item):
+        """Appends an item to the MinList
+        >>> m = MinList()
+        >>> m.append(4)
+        >>> m.append(2)
+        >>> m.size
+        2
+        """
+        self.size += 1
+        for i in range(len(self.items)):
+            if self.items[i] > item:
+                self.items = self.items[:i] + [item] + self.items[i:]
+                return
+        self.items.append(item)
+
+    def pop(self):
+        """ Removes and returns the smallest item from the MinList
+        >>> m = MinList()
+        >>> m.append(4)
+        >>> m.append(1)
+        >>> m.append(5)
+        >>> m.pop()
+        1
+        >>> m.size
+        2
+        """
+        minimum = self.items[0]
+        self.items, self.size = self.items[1:], self.size - 1
+        return minimum
+
+
+class Pet():
+    def __init__(self, name, owner):
+        self.is_alive = True  # It's alive!!!
+        self.name = name
+        self.owner = owner
+
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+
+    def talk(self):
+        print(self.name)
+
+
+class Dog(Pet):
+    def talk(self):
+        print(self.name + ' says woof!')
+
+
+class Cat(Pet):
+    def __init__(self, name, owner, lives=9):
+        Pet.__init__(self, name, owner)
+        self.lives = lives
+
+    def talk(self):
+        """ Print out a cat's greeting.
+        >>> Cat('Thomas', 'Tammy').talk()
+        Thomas says meow!
+        """
+        print(self.name, 'says meow!')
+
+    def lose_life(self):
+        """Decrements a cat's life by 1. When lives reaches zero, 'is_alive'
+        becomes False. If this is called after lives has reached zero, print out
+        that the cat has no more lives to lose.
+        """
+        if self.lives == 0:
+            print(self.name, 'has no more lives to lose!')
+            return
+        self.lives -= 1
+        if self.lives == 0:
+            self.is_alive = False
+
+class NoisyCat(Cat): # Fill me in!
+    """A Cat that repeats things twice."""
+    def talk(self):
+        """Talks twice as much as a regular cat.
+        >>> NoisyCat('Magic', 'James').talk()
+        Magic says meow!
+        Magic says meow!
+        """
+        for _ in range(2):
+            Cat.talk(self)
+    def __repr__(self):
+        """The interpreter-readable representation of a NoisyCat
+        >>> muffin = NoisyCat('Muffin', 'Catherine')
+        >>> repr(muffin)
+        "NoisyCat('Muffin', 'Catherine')"
+        >>> muffin
+        NoisyCat('Muffin', 'Catherine')
+        """
+        return 'NoisyCat(\'{}\', \'{}\')'.format(self.name, self.owner)
